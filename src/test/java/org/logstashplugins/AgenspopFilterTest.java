3ee5adf7a8bd7e515c22efe23412d938d5f36591
maxmin93 <maxmin93@gmail.com>
2019-07-11 21:36:31 +0900

    added sample-confs

--- src/test/java/org/logstashplugins/AgenspopFilterTest.java
+++ src/test/java/org/logstashplugins/AgenspopFilterTest.java
@@ -9,27 +9,34 @@ import org.junit.Test;
 import org.logstash.plugins.ConfigurationImpl;
 import org.logstash.plugins.ContextImpl;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AgenspopFilterTest {
 
     @Test
     public void testJavaExampleFilter() {
-        String sourceField = "foo";
-        Configuration config = new ConfigurationImpl(Collections.singletonMap("source", sourceField));
+        Map<String,Object> params = new HashMap<>();
+        params.put("ids", Collections.unmodifiableList(Arrays.asList( "customerid" )));
+        params.put("label", "customers");
+        params.put("datasource", "northwind");
+
+        Configuration config = new ConfigurationImpl(Collections.unmodifiableMap(params));
         Context context = new ContextImpl(null, null);
         AgenspopFilter filter = new AgenspopFilter("test-id", config, context);
 
         Event e = new org.logstash.Event();
+        e.setField("customerid", "BITNINE");
+        e.setField("country", "KR");
+        e.setField("email", "agraph@bitnine.net");
+
         TestMatchListener matchListener = new TestMatchListener();
-        e.setField(sourceField, "abcdef");
         Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
 
         Assert.assertEquals(1, results.size());
-        Assert.assertEquals("fedcba", e.getField(sourceField));
         Assert.assertEquals(1, matchListener.getMatchCount());
+        Assert.assertEquals("northwind_customers_BITNINE", e.getField("id"));
+        Assert.assertEquals(3, ((List<Object>) e.getField("properties")).size() );
     }
 }
 
