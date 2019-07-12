84ea665b20edb82ccb66d0dd3cb780da9b390c9c
Byeong-Guk Min <maxmin93@gmail.com>
2019-07-12 13:52:33 +0900

    added index reset script

---
+++ samples/agenspop-reset-vertex.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+ES_URL="localhost:8087"
+ES_IDX="agensvertex"
+
+echo -e "\n** 0) delete index : ${ES_URL}/${ES_IDX}"
+curl -X DELETE "${ES_URL}/${ES_IDX}"
+sleep 0.5
+
+echo -e "\n** 1) create index : ${ES_URL}/${ES_IDX}"
+curl -X PUT "${ES_URL}/${ES_IDX}" -H 'Content-Type: application/json' -d'
+{}
+'
+sleep 0.5
+
+echo -e "\n** 2) put mapping : ${ES_URL}/${ES_IDX}"
+curl -X PUT "${ES_URL}/${ES_IDX}/_mapping/${ES_IDX}" -H 'Content-Type: application/json' -d'
+{
+    "properties": {
+        "datasource": {
+            "type": "keyword"
+        },
+        "deleted": {
+            "type": "boolean"
+        },
+        "id": {
+            "type": "text",
+            "fields": {
+                "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256
+                }
+            }
+        },
+        "label": {
+            "type": "keyword"
+        },
+        "properties": {
+            "type": "nested",
+            "include_in_parent": true,
+            "properties": {
+                "key": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 256
+                        }
+                    }
+                },
+                "type": {
+                    "type": "text",
+                    "fields": {
+                        "keyword":
+                        {
+                            "type": "keyword",
+                            "ignore_above": 256
+                        }
+                    }
+                },
+                "value": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 256
+                        }
+                    }
+                }
+            }
+        },
+        "version": {
+            "type": "long"
+        }
+    }
+}
+'
+sleep 0.5
+
+echo -e "\n** 3) check index : ${ES_URL}"
+curl -X GET "${ES_URL}/${ES_IDX}?pretty=true"
+
+echo -e "\n  ..done, Good-bye\n"
